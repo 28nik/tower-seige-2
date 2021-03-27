da431443d50bbab2843a1c7f5568d588ab27423d
28nik <73457837+28nik@users.noreply.github.com>
2021-03-27 15:09:11 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,33 @@
+class Box{
+  constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.4,
+          'friction':0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+
+      World.add(world, this.body);
+    }
+    
+    display(){
+      
+      
+
+        if(this.body.speed < 5){
+          push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        strokeWeight(2);
+        stroke(0);
+        rect(0, 0, this.width, this.height);
+        pop();
+        }
+        else{
+          World.remove(world,this.body)
+        }
+      
+      
+    }
+  }
